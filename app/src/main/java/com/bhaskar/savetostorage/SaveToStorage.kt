f9e13af309419054e3dfe310f092818d259d2bdb
Bhaskar Narayan <krbn1999@gmail.com>
2022-10-12 13:38:27 +0530

    feat(): Bitmap conversion, Text file save

--- app/src/main/java/com/bhaskar/savetostorage/SaveToStorage.kt
+++ app/src/main/java/com/bhaskar/savetostorage/SaveToStorage.kt
@@ -2,6 +2,7 @@ package com.bhaskar.savetostorage
 
 import android.app.Activity
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.util.Log
 import java.io.File
 import java.io.FileOutputStream
@@ -43,23 +44,54 @@ class SaveToStorage (private val activity: Activity) {
     }
 
     /**
-     * Function which starts the process to save the image-file
+     * Function to create target file instance
      */
-    fun saveImageToStorage(directoryName: String, fileName: String, imageBitmap: Bitmap) {
+    private fun getFile(directoryName: String, fileName: String): File {
         val outputDirectory: File = setOutputDirectory(directoryName)
         val targetFile = File("$outputDirectory/$fileName")
         if (targetFile.exists()) {
             targetFile.delete()
         }
+        return targetFile
+    }
+
+    /**
+     * Function to convert ByteArray into Bitmap
+     */
+    fun byteArrayToBitmap(imageByteArray: ByteArray): Bitmap {
+        return BitmapFactory.decodeByteArray(imageByteArray, 0, imageByteArray.size)
+    }
+
+    /**
+     * Function which starts the process to save the image-file
+     */
+    fun saveImageToStorage(directoryName: String, fileName: String, imageBitmap: Bitmap) {
+        val targetFile = getFile(directoryName, fileName)
         try {
             val out = FileOutputStream(targetFile)
             imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, out)
             out.flush()
             out.close()
-            onSaveListener?.onSaveSuccess("$outputDirectory/$fileName")
+            onSaveListener?.onSaveSuccess(targetFile.absolutePath)
         } catch (e: Exception) {
             onSaveListener?.onSaveFail(e)
             Log.d(TAG, "saveImageToStorage: ${e.message}")
+        }
+    }
+
+    /**
+     * Function to save content in a Text file
+     */
+    fun saveTextFileToStorage(text: String, directoryName: String, fileName: String) {
+        val targetFile = getFile(directoryName, "$fileName.txt")
+        try {
+            val out = FileOutputStream(targetFile)
+            out.write(text.toByteArray())
+            out.flush()
+            out.close()
+            onSaveListener?.onSaveSuccess(targetFile.absolutePath)
+        } catch (e: Exception) {
+            onSaveListener?.onSaveFail(e)
         }
     }
 
